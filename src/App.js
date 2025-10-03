edaaa907ff0e55a7ac63e753d403e8c50a3323a3
CareerEngineer <careerengineeringlab@gmail.com>
2025-10-04 01:58:54 +0900

    Update

--- src/App.js
+++ src/App.js
@@ -339,8 +339,6 @@ function CareerStatementGenerator() {
               <p className="text-xs text-red-500 mb-1">문서의 전체 또는 일부를 저작권자의 사전 서면 동의 없이 무단으로 복제, 배포, 전송, 전시, 방송하거나</p>
               <p className="text-xs text-red-500 mb-1">수정 및 편집하는 행위는 금지되어 있으며, 위반 시 관련 법령에 따라 법적인 책임을 질 수 있습니다.</p>
               <p className="text-xs text-gray-500">오직 개인적인 용도로만 사용해야 하며, 상업적 목적의 사용 및 무단 배포를 엄격히 금지합니다.</p>
-</p>
-</p>
             </div>
 
 
